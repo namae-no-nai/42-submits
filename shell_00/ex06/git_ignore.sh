52f3c8df12861d5e519a1796ac35ccad6aed8602
guilherme bertho luiz <guilhermebluiz@hotmail.com>
2019-11-20 17:06:07 -0300

    shell

---
+++ shell_00/ex06/git_ignore.sh
@@ -0,0 +1,2 @@
+git check-ignore *
+git check-ignore .*
---
+++ shell_01/ex07/r_dwssap.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat /etc/passwd | grep  "*" | sed -n "n;p" | cut -d ':' -f 1 | rev | sort -r | sed -n -e "$FT_LINE1,${FT_LINE2}p" | sed 's/$/, /' | tr -d '\n' | sed 's/, $/./' | tr -d '\n'
